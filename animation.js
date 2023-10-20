0704e18504fd1477da31a8b99ecb168e788947c4
Aparna-2005 <aparnaravi205@gmail.com>
2023-10-20 20:47:52 +0530

    added

---
+++ animation.js
@@ -0,0 +1,16 @@
+let canvas = document.getElementById("my_canvas");
+let context = canvas.getContext("2d");
+
+const speed = 4;
+let position = 0;
+let moveSpeed = speed;
+let radius = 40;
+
+function moveBall(){
+    if(position + radius > 640){
+        moveSpeed =  -speed;
+    }elseif(position - radius <0){
+        moveSpeed = speed;
+    }
+        
+    }
